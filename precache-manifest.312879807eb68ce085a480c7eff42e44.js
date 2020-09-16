7fbe50cc4e1ad82ea9582079aaed29bfdb05692f
squeeton <30373837+squeeton@users.noreply.github.com>
2020-09-15 20:20:22 -0500

    Updates

---
+++ precache-manifest.312879807eb68ce085a480c7eff42e44.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "347f6041c6a6756b5cb6d5866d5d4f17",
+    "url": "/among-us-detective/index.html"
+  },
+  {
+    "revision": "375d328e0c72c6f3229a",
+    "url": "/among-us-detective/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "32517ea1e708ac77f71f",
+    "url": "/among-us-detective/static/css/main.d7629629.chunk.css"
+  },
+  {
+    "revision": "375d328e0c72c6f3229a",
+    "url": "/among-us-detective/static/js/2.c7c7325d.chunk.js"
+  },
+  {
+    "revision": "91e63cf6c7fddcff4eb03767a3e3fc2d",
+    "url": "/among-us-detective/static/js/2.c7c7325d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32517ea1e708ac77f71f",
+    "url": "/among-us-detective/static/js/main.2fd33d45.chunk.js"
+  },
+  {
+    "revision": "d11125e66171fd02171d",
+    "url": "/among-us-detective/static/js/runtime-main.d95c2edf.js"
+  },
+  {
+    "revision": "b92651584697c9e88184a234f6ff52e9",
+    "url": "/among-us-detective/static/media/mira.b9265158.png"
+  },
+  {
+    "revision": "8623271ec7717bfd58d1141337c5a19b",
+    "url": "/among-us-detective/static/media/polus.8623271e.png"
+  },
+  {
+    "revision": "9c61140045d9a9ce78ff2a7fc5f0888d",
+    "url": "/among-us-detective/static/media/skeld.9c611400.png"
+  }
+]);
